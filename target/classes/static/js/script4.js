7151882e7251c6bc909fed257ac92582d8e84e66
Medhansh Sharma <sharmamedhansh21@gmail.com>
2024-11-11 21:35:06 +0530

    render production hosting

---
+++ target/classes/static/js/script4.js
@@ -0,0 +1,31 @@
+const themeLink = document.getElementById('theme-style');
+
+// Function to switch between light and dark themes
+function toggleTheme() {
+    const currentTheme = themeLink.getAttribute('href');
+    if (currentTheme.includes('dark')) {
+        themeLink.setAttribute('href', '/css/style.css');
+        localStorage.setItem('theme', 'light');
+    }
+    else {
+        themeLink.setAttribute('href', '/css/style-dark.css');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+// Add event listeners for both PC and mobile theme toggles
+// document.getElementById('theme-toggle').addEventListener('click', toggleTheme);
+// document.getElementById('theme-toggles').addEventListener('click', toggleTheme);
+
+// Load saved theme from localStorage on page load
+window.addEventListener('load', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'light') {
+        themeLink.setAttribute('href', '/css/style.css');
+    } else {
+        themeLink.setAttribute('href', '/css/style-dark.css');
+    }
+});
+function goBack() {
+    location.href = "/getlinks";
+};
